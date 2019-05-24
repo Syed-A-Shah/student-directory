318c9c45f40ccfd795ca58d0da2f0d62badbe4bd
EC2 Default User <ec2-user@ip-172-31-24-247.us-east-2.compute.internal>
2019-05-24 11:39:41 +0000

    Removed hard-coded names, Added method for user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-# First, we put all of the students into an array
-students = [
-  { name: 'Dr. Hannibal Lecter', cohort: :november },
-  { name: 'Darth Vader', cohort: :november },
-  { name: 'Nurse Ratched', cohort: :november },
-  { name: 'Michael Corleone', cohort: :november },
-  { name: 'Alex DeLarge', cohort: :november },
-  { name: 'The Wicked Witch of the West', cohort: :november },
-  { name: 'Terminator', cohort: :november },
-  { name: 'Freddy Krueger', cohort: :november },
-  { name: 'The Joker', cohort: :november },
-  { name: 'Joffrey Baratheon', cohort: :november },
-  { name: 'Norman Bates', cohort: :november }
-]
+def input_students
+  puts "Please enter the name of the student"
+  puts "To finish, just hit return twice"
+# Create an empty array
+  students = []
+# Get the first name
+  name = gets.chomp
+# While the name is not empty, repeat this code
+  while !name.empty? do
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+#   Get another name from the user
+    name = gets.chomp
+  end
+#  Return the array of students
+  students
+end
+  
+
 # then we print them
 def print(students)
   students.each do |student|
@@ -29,6 +34,7 @@ def print_footer(students)
 end
 
 # nothing happend until we call each method
+students = input_students
 print_header
 print(students)
 print_footer(students)
