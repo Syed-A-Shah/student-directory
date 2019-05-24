90e09af23ca80aff4dbc53fbadbfc09e8485f3dd
EC2 Default User <ec2-user@ip-172-31-24-247.us-east-2.compute.internal>
2019-05-24 11:24:35 +0000

    Added Hash with name & cohort

--- directory.rb
+++ directory.rb
@@ -1,20 +1,22 @@
 # First, we put all of the students into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  { name: 'Dr. Hannibal Lecter', cohort: :november },
+  { name: 'Darth Vader', cohort: :november },
+  { name: 'Nurse Ratched', cohort: :november },
+  { name: 'Michael Corleone', cohort: :november },
+  { name: 'Alex DeLarge', cohort: :november },
+  { name: 'The Wicked Witch of the West', cohort: :november },
+  { name: 'Terminator', cohort: :november },
+  { name: 'Freddy Krueger', cohort: :november },
+  { name: 'The Joker', cohort: :november },
+  { name: 'Joffrey Baratheon', cohort: :november },
+  { name: 'Norman Bates', cohort: :november }
 ]
 # then we print them
-students.each do |student|
-  puts student
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_header
@@ -22,15 +24,11 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
-  end
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
+
 # nothing happend until we call each method
 print_header
 print(students)
-print_footer(students) 
+print_footer(students)
