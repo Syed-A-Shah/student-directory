af19f51f8d3793a47198d5a09fb0d1fe8328fed9
EC2 Default User <ec2-user@ip-172-31-24-247.us-east-2.compute.internal>
2019-05-28 00:41:09 +0000

    Country of birth

--- test_directory.rb
+++ test_directory.rb
@@ -1,40 +1,43 @@
+# let's ask the user for student names
 def input_students
-  puts 'Please enter the names of the students'
-  puts 'To finish, just hit return twice'
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << { name: name, cohort: :november }
+    students << {name: name, cohort: :november}
+    # ask for student's country of birth
+    student_num = students.count - 1
+    puts "What is #{students[student_num][:name]}'s country of birth?"
+    students[student_num][:country_of_birth] = gets.chomp
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
+  # now
   # return the array of students
   students
 end
 
 def print_header
-  puts 'The students of Villains Academy'
-  puts '-------------'
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
- count = 0
-  while count < students.length do
-    puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
-    count += 1
+  students.each.with_index do |student, idx|
+    puts "#{idx + 1}. #{student[:name]} (#{student[:country_of_birth]}, #{student[:cohort].capitalize} cohort)"
   end
 end
- 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
+#nothing happens until we call the methods
 students = input_students
-# nothing happens until we call each method
 print_header
 print(students)
 print_footer(students)
